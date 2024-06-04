48511fe4f33a08b5756f0c4ca1ba79b5863c1b12
Sanket Singh <singhsanket143@gmail.com>
2024-06-04 21:08:30 +0530

    Added connect db call

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const fastify = require('fastify')({ logger: true }); // calling the fastify constructor
 const app = require('./app');
+const connectToDB = require('./config/dbConfig');
 const serverConfig = require('./config/serverConfig');
 
 fastify.register(app);
 
-fastify.listen({ port: serverConfig.PORT }, (err) => {
+fastify.listen({ port: serverConfig.PORT }, async (err) => {
     if (err) {
       fastify.log.error(err);
       process.exit(1)
     }
+    await connectToDB();
     console.log(`Server up at port ${serverConfig.PORT}`);
 });
